222b6efa34371b4016a0477e9d515bda24e0a340
Francisco Sokol <francisco.sokol@otto.de>
2019-04-24 12:04:32 +0200

    Clear warnings

--- nummus-fe/src/main/js/application/components/MonthSelector.js
+++ nummus-fe/src/main/js/application/components/MonthSelector.js
@@ -1,25 +1,17 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import moment from 'moment';
-import CategoryRepository from "../../domain/CategoryRepository";
 import BudgetRepository from "../../domain/BudgetRepository";
-import BudgetInput from "./BudgetInput";
-import AmountFormatter from "../AmountFormatter";
 
 class MonthSelector extends Component {
   static propTypes = {
     budgetRepository: PropTypes.instanceOf(BudgetRepository),
   };
 
-  constructor(props) {
-    super(props);
-  }
-
-
   render() {
     let options = this.props.budgetRepository.listMonths().map(month => {
       let date = moment(month, "YYYY_MM").format("MMM YY");
-      return <option value={month}>{date}</option>
+      return <option value={month} key={month}>{date}</option>
     });
     return (
       <div>
