3c11c1a9fad6540b95b2d3cc0d5fdec5ec5cad65
Francisco Sokol <francisco.sokol@otto.de>
2019-04-26 01:25:27 +0200

    Updates

---
+++ precache-manifest.95769d492c09c1a9e363aff9d14ebe38.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e61b8ac3d0e20bdc18c16036c536b4db",
+    "url": "/nummus/index.html"
+  },
+  {
+    "revision": "a6d87eee6dab13334369",
+    "url": "/nummus/static/css/main.5f012466.chunk.css"
+  },
+  {
+    "revision": "8e1a66dfaf2c69c66f45",
+    "url": "/nummus/static/js/2.85157427.chunk.js"
+  },
+  {
+    "revision": "a6d87eee6dab13334369",
+    "url": "/nummus/static/js/main.d95342b4.chunk.js"
+  },
+  {
+    "revision": "bbbaea70b4959c2c489c",
+    "url": "/nummus/static/js/runtime~main.ca79f64e.js"
+  }
+]);
